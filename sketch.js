5547bf11731a66a29260ab86d7a68c5063185856
JojoDz <72552227+JojoDz@users.noreply.github.com>
2021-03-05 19:39:12 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+
+function decode_morse(morse_code){
+    //chaîne de caractère dans un tableau à chaque espace
+    morse_code_tab = morse_code.split(" ");
+    //endroit où on va stocker les éléments convertis
+    let solution = "";
+    morse_code_tab.forEach(element => {
+        let print = compare_alphabet_morse(element);
+        //chaque mot en morse est converti en chaine de caractère mais sur des lignes différentes
+        console.log(element);
+        solution += print;
+        //caractère converti sur une ligne
+    });
+    console.log(solution);
+}
+
+decode_morse("-... --- -. .--- ..-   .- -.. .-");
+
+function compare_alphabet_morse(morse_letter){
+    var morseAlphabet = {
+        //problématique de deux espaces qu'il ne reconnaît pas
+        //tableau lettre
+        //tableau mots
+        " ": " ",
+        ".-": "a",
+        "-...":"b",
+        "-.-.": "c",
+        "-..": "d",
+        ".":"e",
+        "..-.":"f",
+        "--.":"g",
+        "....":"h",
+        "..":"i",
+        ".---":"j",
+        "-.-":"k",
+        ".-..":"l",
+        "--":"m",
+        "-.":"n",
+        "---":"o",
+        ".--.":"p",
+        "--.-":"q",
+        ".-.":"r",
+        "...":"s",
+        "-":"t",
+        "..-":"u",
+        "...-":"v",
+        ".--":"w",
+        "-..-":"x",
+        "-.--":"y",
+        "--..":"z",
+        ".----":"1", 
+        "..---":"2", 
+        "...--":"3",
+        "....-":"4", 
+        ".....":"5", 
+        "-....":"6", 
+        "--...":"7", 
+        "---..":"8", 
+        "----.":"9", 
+        "-----":"0", 
+        "|":" "
+        //clé avec valeur
+    }
+    let convertedLetter = morseAlphabet[morse_letter];
+    return convertedLetter;
+}
